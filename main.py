4f7c2a74755b3ce3f0dc8d40e9c9cb2d7749ef1d
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2023-05-18 23:41:03 +0100

    Added get_page() function

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import requests
+
+
+def get_page(url: str) -> str:
+    """
+    Retrieves the html content of a url
+    @param url: URL to request
+    @return: html content
+    """
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.text
+    else:
+        print(f"Error retrieving webpage. Status code: {response.status_code}")
+        return ""
+
 def main():
     print(
     "====================================================================================\n\n"+

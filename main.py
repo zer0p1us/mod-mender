e4761340f84677a123cd6e4b2fd98e9847a23ded
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2023-05-22 02:07:26 +0100

    Added load_mods_list()

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import json
+from typing import Dict
 
 
 def get_page(url: str) -> str:
@@ -14,6 +16,15 @@ def get_page(url: str) -> str:
     else:
         print(f"Error retrieving webpage. Status code: {response.status_code}")
         return ""
+
+def load_mods_list(path: str) -> Dict:
+    """
+    Loads JSON data into dictionary
+    @param path: file path to the mod_list.json file
+    @return: dictionary with the json content of mod_list.json
+    """
+    file = open('mod_list.json')
+    return json.load(file)
 
 def main():
     print(

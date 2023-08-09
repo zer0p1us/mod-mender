ac46281e6cc28a89792c444cff59689f1ffcf680
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2023-08-09 13:37:41 +0000

    Fixed error output

--- src/mod_mender/main.py
+++ src/mod_mender/main.py
@@ -156,10 +156,10 @@ def main(argv: list[str] = sys.argv):
     try:
         mod_list_data = load_mods_list(mod_list_file)
     except FileNotFoundError:
-        print("Couldn't find {mod_list_file}")
+        print(f"Couldn't find {mod_list_file}")
         sys.exit(-1)
     except Exception:
-        print("Couldn't open {mod_list_file}")
+        print(f"Couldn't open {mod_list_file}")
         sys.exit(-1)
 
     mods = mod_list_data["mods"]
